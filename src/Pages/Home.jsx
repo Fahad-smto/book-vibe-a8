b5db3be89459bc1ea5367de9e8852c08d939ab52
Fahad-smto <simanto.smto.prog@gmail.com>
2024-03-26 00:44:24 +0600

    setup done and navbar added

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import NavBar from "../Components/NavBar";
+
+ 
+
+const Home = () => {
+    return (
+        <div>
+            <NavBar></NavBar>
+            <h1>homePage</h1>
+        </div>
+    );
+};
+
+export default Home;
